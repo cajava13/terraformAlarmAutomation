e9747b2da2cd836195bf741eb7711b7751badfac
cajava13 <43886531+cajava13@users.noreply.github.com>
2023-11-02 17:44:38 -0300

    latest version documents

--- createAlarm.py
+++ createAlarm.py
@@ -4,31 +4,6 @@ import boto3
 class CreateAlarm:
     def __init__(self):
         self.cloudwatch_client = boto3.client('cloudwatch')
-        self.sns = boto3.client('sns')
-    
-    def find_topic(self):
-        # List all of the SNS topics
-        topics = self.sns.list_topics()
-
-        # Iterate through the list of topics and find the topic that we want to use
-        topic_arn = None
-        for topic in topics['Topics']:
-            topic_arn = topic['TopicArn']
-            if topic_arn == 'arn:aws:sns:us-east-1:165834741047:MyTopic':
-                break
-
-        # If we didn't find the topic, then return an error
-        if topic_arn is None:
-            return {
-                'statusCode': 404,
-                'body': 'Topic not found'
-            }
-
-        # Return the topic ARN
-        return {
-            'statusCode': 200,
-            'body': topic_arn
-        }
              
     def create_alarm(self,InstanceID):
         try:
@@ -39,6 +14,7 @@ class CreateAlarm:
                 MetricName='CPUUtilization',
                 Namespace='AWS/EC2',
                 Statistic='Average',
+                AlarmActions=['arn:aws:sns:us-east-1:024830515316:MyTopic'],
                 Period=60,
                 EvaluationPeriods=1,
                 Threshold=80,
